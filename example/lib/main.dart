0c27cc77a1759c01bc376f581ab78fb93dda3a28
Hugo Passos <hugocbpassos@gmail.com>
2019-09-22 21:24:40 -0300

    Update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:drag_select_grid_view/drag_select_grid_view.dart';
 import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(statusBarColor: Colors.grey[200]),
+  );
+  runApp(
+    MaterialApp(
+      home: MyApp(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        accentColor: Colors.white,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(elevation: 1),
+      ),
+    ),
+  );
+}
 
 class MyApp extends StatefulWidget {
   @override
@@ -11,53 +27,17 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   @override
-  void initState() {
-    super.initState();
-    changeStatusBarColorIfIsAndroid();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.white,
-        accentColor: Colors.white,
-        scaffoldBackgroundColor: Colors.white,
-        appBarTheme: AppBarTheme(elevation: 1),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Drag Select Grid View Example'),
+        centerTitle: true,
       ),
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Drag Select Grid View Sample'),
-          centerTitle: true,
-        ),
-        body: Stack(
-          children: [
-            dragSelectGridView(),
-            autoScrollHotspotIndicator(isUpperHotspot: true),
-            autoScrollHotspotIndicator(isUpperHotspot: false),
-          ],
-        ),
-      ),
+      body: buildDragSelectGridView(),
     );
   }
 
-  Positioned autoScrollHotspotIndicator({@required bool isUpperHotspot}) {
-    return Positioned(
-      top: isUpperHotspot ? 0 : null,
-      right: 0,
-      bottom: isUpperHotspot ? null : 0,
-      left: 0,
-      height: DragSelectGridView.defaultAutoScrollHotspotHeight,
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.grey[300].withOpacity(0.5),
-        ),
-      ),
-    );
-  }
-
-  DragSelectGridView dragSelectGridView() {
+  DragSelectGridView buildDragSelectGridView() {
     return DragSelectGridView(
       padding: EdgeInsets.all(8),
       itemCount: 90,
@@ -90,14 +70,6 @@ class _MyAppState extends State<MyApp> {
         ),
       ),
     );
-  }
-
-  void changeStatusBarColorIfIsAndroid() {
-    if (Theme.of(context).platform == TargetPlatform.android) {
-      SystemChrome.setSystemUIOverlayStyle(
-        SystemUiOverlayStyle(statusBarColor: Colors.grey[200]),
-      );
-    }
   }
 }
 
