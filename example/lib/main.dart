b9345e40b07420c13a01568451f03525c608ea35
Hugo Passos <hugocbpassos@gmail.com>
2019-09-29 23:38:24 -0300

    Arrange the example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:drag_select_grid_view/drag_select_grid_view.dart';
-import 'package:flutter/services.dart';
+
+import 'selectable_item.dart';
+import 'selection_app_bar.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(
@@ -14,7 +17,7 @@ void main() {
         primaryColor: Colors.white,
         accentColor: Colors.white,
         scaffoldBackgroundColor: Colors.white,
-        appBarTheme: AppBarTheme(elevation: 1),
+        appBarTheme: const AppBarTheme(elevation: 2),
       ),
     ),
   );
@@ -26,132 +29,46 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Drag Select Grid View Example'),
-        centerTitle: true,
-      ),
-      body: buildDragSelectGridView(),
-    );
-  }
-
-  DragSelectGridView buildDragSelectGridView() {
-    return DragSelectGridView(
-      padding: EdgeInsets.all(8),
-      itemCount: 90,
-      itemBuilder: (BuildContext context, int index, bool selected) {
-        return ExampleChildItem(
-          index: index,
-          color: Colors.blue,
-          selected: selected,
-        );
-      },
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 150,
-        crossAxisSpacing: 8,
-        mainAxisSpacing: 8,
-      ),
-    );
-  }
-
-  Widget gridViewItem(int index) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(2),
-        color: Colors.blue,
-      ),
-      child: Center(
-        child: Text(
-          'Item\n#$index',
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 22, color: Colors.white),
-        ),
-      ),
-    );
-  }
-}
-
-class ExampleChildItem extends StatefulWidget {
-  const ExampleChildItem({
-    Key key,
-    @required this.index,
-    @required this.color,
-    @required this.selected,
-  }) : super(key: key);
-
-  final int index;
-  final MaterialColor color;
-  final bool selected;
-
-  @override
-  _ExampleChildItemState createState() => _ExampleChildItemState();
-}
-
-class _ExampleChildItemState extends State<ExampleChildItem>
-    with SingleTickerProviderStateMixin {
-  AnimationController _controller;
-  Animation<double> _scaleAnimation;
+  final controller = DragSelectGridViewController();
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
-      value: widget.selected ? 1.0 : 0.0,
-      duration: kThemeChangeDuration,
-      vsync: this,
-    );
-    _scaleAnimation =
-        Tween<double>(begin: 1.0, end: 0.8).animate(CurvedAnimation(
-      parent: _controller,
-      curve: Curves.ease,
-    ));
-  }
-
-  @override
-  void didUpdateWidget(ExampleChildItem oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (oldWidget.selected != widget.selected) {
-      if (widget.selected) {
-        _controller.forward();
-      } else {
-        _controller.reverse();
-      }
-    }
+    controller.addListener(scheduleRebuild);
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    controller.removeListener(scheduleRebuild);
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _scaleAnimation,
-      builder: (BuildContext context, Widget child) {
-        final color = Color.lerp(
-            widget.color.shade500, widget.color.shade900, _controller.value);
-        return Transform.scale(
-          scale: _scaleAnimation.value,
-          child: DecoratedBox(
-            decoration: BoxDecoration(color: color),
-            child: child,
-          ),
-        );
-      },
-      child: Container(
-        alignment: Alignment.center,
-        child: Text(
-          '${widget.index}',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 22.0,
-          ),
+    return Scaffold(
+      appBar: SelectionAppBar(
+        selection: controller.selection,
+        title: const Text('Grid Example'),
+      ),
+      body: DragSelectGridView(
+        gridController: controller,
+        padding: EdgeInsets.all(8),
+        itemCount: 90,
+        itemBuilder: (context, index, selected) {
+          return SelectableItem(
+            index: index,
+            color: Colors.blue,
+            selected: selected,
+          );
+        },
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 150,
+          crossAxisSpacing: 8,
+          mainAxisSpacing: 8,
         ),
       ),
     );
   }
+
+  void scheduleRebuild() => setState(() {});
 }
---
+++ example/lib/selection_app_bar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:drag_select_grid_view/drag_select_grid_view.dart';
+
+class SelectionAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const SelectionAppBar({
+    Key key,
+    this.title,
+    this.selection = Selection.empty,
+  })  : assert(selection != null),
+        super(key: key);
+
+  final Widget title;
+  final Selection selection;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSwitcher(
+      duration: kThemeAnimationDuration,
+      child: selection.isSelecting
+          ? AppBar(
+              key: Key('selecting'),
+              titleSpacing: 0,
+              leading: const CloseButton(),
+              title: Text(
+                '${selection.amount} item(s) selected…',
+                key: Key('${selection.amount}'),
+              ),
+            )
+          : AppBar(
+              key: Key('not-selecting'),
+              titleSpacing: NavigationToolbar.kMiddleSpacing,
+              title: title,
+            ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
