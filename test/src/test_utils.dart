8938ac47ff38eaf096227e0f84e93087f0e35c9d
Hugo Passos <hugocastelanibp@gmail.com>
2020-08-08 20:20:42 -0300

    Bump dependencies versions and remove direct `meta` dependency

--- test/src/test_utils.dart
+++ test/src/test_utils.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:meta/meta.dart';
 
 /// Flutter's default value for `testWidgets()`.
 const screenHeight = 600.0;
